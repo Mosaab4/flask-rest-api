ff291c0af6f7a43a2e9651907fd84fd093afc1f5
mosaab <musab.ehab@gmail.com>
2018-06-11 23:24:53 +0200

    complete project3

--- project2/app.py
+++ project2/app.py
@@ -1,5 +1,5 @@
 from flask import Flask,request
-from flask_restful import Resource,Api
+from flask_restful import Resource,Api, reqparse
 from security import authenticate, identity
 from flask_jwt import JWT, jwt_required
 
@@ -14,15 +14,27 @@ items = [
 
 ]
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'price',
+        type=float,
+        required = True,
+        help="This field cannot be left blank"
+    )
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name']== name , items), None)
         return {'item': item}, 200 if item else 404
 
     def post(self, name):
+
+        
         if next(filter(lambda x: x['name']== name , items), None):
             return {'message':"an item with {} already exists".format(name)}, 400
-        data = request.get_json()
+        
+        data = Item.parser.parse_args()
+        
         item ={
             'name':name,
             'price': data['price']
@@ -37,7 +49,8 @@ class Item(Resource):
         return {'message':'Item deleted'}
 
     def put(self, name):
-        data = request.get_json()
+
+        data = Item.parser.parse_args()
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None :
             item = {
