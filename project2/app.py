85ac5ea341d645cffd373bfbcb33d43ec5a5e35e
mosaab <musab.ehab@gmail.com>
2018-06-11 21:39:01 +0200

    add files to project2

--- project2/app.py
+++ project2/app.py
@@ -3,6 +3,7 @@ from flask_restful import Resource,Api
 
 
 app = Flask(__name__)
+app.secret_key = 'ADF#S$%^^%&HFGFGZXC)I#$'
 api = Api(app)
 
 items = [
@@ -10,12 +11,12 @@ items = [
 ]
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
+        item = next(filter(lambda x: x['name']== name , items), None)
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
+        if next(filter(lambda x: x['name']== name , items), None):
+            return {'message':"an item with {} already exists".format(name)}, 400
         data = request.get_json()
         item ={
             'name':name,
