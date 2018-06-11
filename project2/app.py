18d3b855ea6cbfdc3e48f2a06e4fbb0bf4f3cce3
mosaab <musab.ehab@gmail.com>
2018-06-11 23:03:22 +0200

    finish project3

--- project2/app.py
+++ project2/app.py
@@ -1,15 +1,20 @@
 from flask import Flask,request
 from flask_restful import Resource,Api
+from security import authenticate, identity
+from flask_jwt import JWT, jwt_required
 
 
 app = Flask(__name__)
 app.secret_key = 'ADF#S$%^^%&HFGFGZXC)I#$'
 api = Api(app)
 
+jwt = JWT(app, authenticate,identity)
+
 items = [
 
 ]
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name']== name , items), None)
         return {'item': item}, 200 if item else 404
@@ -25,6 +30,28 @@ class Item(Resource):
         items.append(item)
 
         return item, 201
+    
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message':'Item deleted'}
+
+    def put(self, name):
+        data = request.get_json()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None :
+            item = {
+                'name': name,
+                'price': data['price']
+            }
+            items.append(item)
+        else:
+            item.update(item)
+
+        return item
+
+
+
 
 class ItemList(Resource):
     def get(self):
--- project2/security.py
+++ project2/security.py
@@ -1,33 +1,16 @@
+from werkzeug.security import safe_str_cmp
 from user import User
 users = [
-    {
-        'id':1,
-        'username':'mosaab',
-        'password':'mmm234567'
-    }
+    User(1, 'mosaab', 'mmm12345')
 ]
 
-username_mapping = {
-    'mosaab':
-        {
-            'id':1,
-            'username':'mosaab',
-            'password':'mmm234567'
-        }
-}
-userid_mapping = {
-    1 :
-        {
-            'id':1,
-            'username':'mosaab',
-            'password':'mmm234567'
-        }
-}
+username_mapping = {u.username: u for u in users}
+userid_mapping = {u.id : u for u in users}
 
 
 def authenticate(username, password):
     user = username_mapping.get(username, None)
-    if user and user.password == password:
+    if user and safe_str_cmp(user.password , password):
         return user
 
 def identity(payload):
